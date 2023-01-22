613f4025ccca5955a4b62ea2449896d1b11f5aad
db-99 <baj3rdav@gmail.com>
2023-01-22 20:02:14 +0100

    init movie service

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getMovieDetails(): Observable<Object>
+  {
+    return this.http.get('');
+  }
 }
