e23a81e000435b9b243ca3bc444fac4c26ba028c
db-99 <baj3rdav@gmail.com>
2023-01-22 23:47:42 +0100

    get details response after clicking a movie

--- src/app/pages/movie-details/movie-details.page.ts
+++ src/app/pages/movie-details/movie-details.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MovieService } from 'src/app/services/movie.service';
 
 @Component({
   selector: 'app-movie-details',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieDetailsPage implements OnInit {
 
-  constructor() { }
+  // inject the current activatedroute - we get information about the part after /
+  constructor(private route: ActivatedRoute, private movieService: MovieService) { }
 
   ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id') ?? "";  // nejaka fixni hodnota kdyby to bylo null, jinak error
+    console.log(id);
+    this.movieService.getMovieDetails(id).subscribe(res => {
+      console.log(res);
+    })
   }
 
 }
