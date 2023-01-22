f0c4bb22f70507d32ba806d0efe64c1644c97581
db-99 <baj3rdav@gmail.com>
2023-01-22 21:04:24 +0100

    add loading controller for search

--- src/app/pages/movies/movies.page.ts
+++ src/app/pages/movies/movies.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
 import { MovieService } from 'src/app/services/movie.service';
 
 @Component({
@@ -7,14 +8,28 @@ import { MovieService } from 'src/app/services/movie.service';
   styleUrls: ['./movies.page.scss'],
 })
 export class MoviesPage implements OnInit {
+  searchResults = [];
+  currentPage = 1;
+  title = 'jurassic';
 
-  constructor(private movieService: MovieService) { }
+  constructor(private movieService: MovieService, private loadingCtrl: LoadingController) { }
 
   ngOnInit() {
+    this.loadMovies();
+  }
+
+  async loadMovies()
+  {
+    const loading = await this.loadingCtrl.create({
+      message: 'Searching...',
+      spinner: 'bubbles',
+    });
+    await loading.present();
     /*this.movieService.getMovieDetails().subscribe((res) => {
       console.log(res);
     });*/
-    this.movieService.searchMovies().subscribe((res) => {
+    this.movieService.searchMovies(this.title, this.currentPage).subscribe((res) => {
+      loading.dismiss();  // po nacteni odebrat nacitaci obrazovku
       //console.log(res.totalResults);  // interfacem se tohle umoznilo
       console.log(res);
     });
