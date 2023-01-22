2f7e3661b14005a7e51c5efed58d289421c6e6b3
db-99 <baj3rdav@gmail.com>
2023-01-22 22:59:01 +0100

    add infinite scrolling until all results shown

--- src/app/pages/movies/movies.page.ts
+++ src/app/pages/movies/movies.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { LoadingController } from '@ionic/angular';
+import { InfiniteScrollCustomEvent, IonInfiniteScroll, LoadingController } from '@ionic/angular';
 import { MovieService, Search } from 'src/app/services/movie.service';
 
 @Component({
@@ -18,7 +18,7 @@ export class MoviesPage implements OnInit {
     this.loadMovies();
   }
 
-  async loadMovies()
+  async loadMovies(event?: InfiniteScrollCustomEvent)
   {
     const loading = await this.loadingCtrl.create({
       message: 'Searching...',
@@ -30,10 +30,25 @@ export class MoviesPage implements OnInit {
     });*/
     this.movieService.searchMovies(this.title, this.currentPage).subscribe((res) => {
       loading.dismiss();  // po nacteni odebrat nacitaci obrazovku
-      this.movies = [...this.movies, ...res.Search];  // nefunguje, vyresit
+      //this.movies = [...this.movies, ...res.Search];  // nefunguje, vyresit
+      this.movies.push(...res.Search);
       //console.log(res.totalResults);  // interfacem se tohle umoznilo
       console.log(res);
+
+      event?.target.complete(); // konec nacitani, zobrazeni dalsich vysledku
+      if (event)
+      {
+        // aby se to nesnazilo nacitat dal i kdyz uz nejsou vysledky
+        // wtf? proc to vraci pocet vysledku ve stringu???
+        event.target.disabled = parseInt(res.totalResults, 10) <= this.currentPage*10;
+      }
     });
+  }
+
+  loadMore(event: any)  // pro nekonecne prohlizeni vysledku vyhledavani, ISCE nahrazeno any kvuli erroru
+  {
+    this.currentPage++;
+    this.loadMovies(event);
   }
 
 }
