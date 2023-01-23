9fc1fe444c3b17ae34a15e2a11627ac5337ba616
db-99 <baj3rdav@gmail.com>
2023-01-23 15:35:35 +0100

    implement basic search function, provisional skeleton text

--- src/app/pages/movies/movies.page.ts
+++ src/app/pages/movies/movies.page.ts
@@ -20,21 +20,51 @@ export class MoviesPage implements OnInit {
 
   loadMovies(loadMore = false, event?: any)
   {
+    // nekde osetrit kdyz je response false, dat totalresults na 0 atd
     if (loadMore)
       this.currentPage++;
 
     this.movieService.getSearchResults(this.title, this.currentPage).subscribe((res) => {
-      if (this.currentPage > 1)
-        this.movies.push(...res.Search);
-      else
-        this.movies = res.Search;
-      console.log(res);
+      if (res.Response == "True") // kdyz neni response, tak neni ani Search[] nebo totalResults
+      {
+        if (this.currentPage > 1)
+          this.movies.push(...res.Search);
+        else
+          this.movies = res.Search;
+        console.log(res);
 
-      event?.target.complete();
-      if (event)
-        event.target.disabled = parseInt(res.totalResults, 10) <= this.currentPage*10;
+        event?.target.complete();
+        if (event)
+          event.target.disabled = parseInt(res.totalResults, 10) <= this.currentPage*10;
+      }
+      else
+      {
+        this.movies = [];
+        if (event)
+            event.target.disabled = true;
+      }
+
 
     });
+  }
+
+  onSearchChange(e:any)
+  {
+    this.title = e.detail.value;  // ve videu jede od indexu dal tak nevadi ze to dava do promenne, ale tady se musi hledat furt to same
+    if (this.title == '')
+    {
+      this.currentPage = 1;
+      this.loadMovies();  // asi zmenit na zobrazeni prazdne stranky nebo neco?
+      return;
+    }
+
+    this.movieService.getSearchResults(this.title, this.currentPage).subscribe((res) => {
+      if (res.Response == "True") // opet potreba osetrit
+        this.movies = res.Search;
+      else
+        this.movies = [];
+    });
+
   }
 
 }
