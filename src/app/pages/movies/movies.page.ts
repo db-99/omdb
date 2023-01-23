bd399141a4d21c5505fe6c4c0a7ba21ac265891c
db-99 <baj3rdav@gmail.com>
2023-01-23 14:39:48 +0100

    change infinite scroll functionality

--- src/app/pages/movies/movies.page.ts
+++ src/app/pages/movies/movies.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { InfiniteScrollCustomEvent, IonInfiniteScroll, LoadingController } from '@ionic/angular';
 import { MovieService, Search } from 'src/app/services/movie.service';
 
@@ -18,37 +18,23 @@ export class MoviesPage implements OnInit {
     this.loadMovies();
   }
 
-  async loadMovies(event?: InfiniteScrollCustomEvent)
+  loadMovies(loadMore = false, event?: any)
   {
-    const loading = await this.loadingCtrl.create({
-      message: 'Searching...',
-      spinner: 'bubbles',
-    });
-    await loading.present();
-    /*this.movieService.getMovieDetails().subscribe((res) => {
-      console.log(res);
-    });*/
+    if (loadMore)
+      this.currentPage++;
+
     this.movieService.getSearchResults(this.title, this.currentPage).subscribe((res) => {
-      loading.dismiss();  // po nacteni odebrat nacitaci obrazovku
-      //this.movies = [...this.movies, ...res.Search];  // nefunguje, vyresit
-      this.movies.push(...res.Search);
-      //console.log(res.totalResults);  // interfacem se tohle umoznilo
+      if (this.currentPage > 1)
+        this.movies.push(...res.Search);
+      else
+        this.movies = res.Search;
       console.log(res);
 
-      event?.target.complete(); // konec nacitani, zobrazeni dalsich vysledku
+      event?.target.complete();
       if (event)
-      {
-        // aby se to nesnazilo nacitat dal i kdyz uz nejsou vysledky
-        // wtf? proc to vraci pocet vysledku ve stringu???
         event.target.disabled = parseInt(res.totalResults, 10) <= this.currentPage*10;
-      }
-    });
-  }
 
-  loadMore(event: any)  // pro nekonecne prohlizeni vysledku vyhledavani, ISCE nahrazeno any kvuli erroru
-  {
-    this.currentPage++;
-    this.loadMovies(event);
+    });
   }
 
 }
