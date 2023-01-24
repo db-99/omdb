26097eef5680a765d21bdc8ff8ae9154905e433b
db-99 <baj3rdav@gmail.com>
2023-01-24 21:21:54 +0100

    add last search functionality without data storage, edit home page html

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
   title: string = '';
+  history = ["1", "2", "3", "4", "5"];
 
   constructor() { }
 
