b95bace8e64f5ca9e4d9af361a536bf78d9f675d
db-99 <baj3rdav@gmail.com>
2023-01-24 22:28:58 +0100

    routing from home done thru function, attempt at saving data

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
 import { StorageService } from 'src/app/services/storage.service';
 
 @Component({
@@ -10,11 +11,14 @@ export class HomePage implements OnInit {
   title: string = '';
   history = ["1", "2", "3", "4", "5"];
 
-  constructor(private storageService: StorageService) {
+  constructor(private router:Router, private storageService: StorageService) {
     this.storageService.getData('searches').then(searches => {
       if (!searches) {
-        searches = [];
+        //searches = [];
+        console.log("zadny searches");
       }
+      console.log("searches existuji");
+      console.log(searches);
       this.history = searches;
     });
   }
@@ -27,10 +31,23 @@ export class HomePage implements OnInit {
     this.title = e.detail.value;
   }
 
-  // udelat routing pres funkci co se spusti po zmacknuti tlacitka, pred routingem ulozit do storage
-  async setHome() {
-    //if (this.history.length == 5)
+  // udelat routing pres funkci co se spusti po zmacknuti tlacitka, pred routingem ulozit do storage (zavolat setHome nebo routing async?)
+  navigateSearch()
+  {
+    this.saveSearch();
+    const params: NavigationExtras = {
+      queryParams: { search: this.title },
+    };
+    this.router.navigate(['/movies'], params);
+  }
+
+  async saveSearch() {
+    if (this.history.length == 5)
+    {
       // posunout vse o 1, na konec pridat posledni search
+      this.history.splice(0, 0, this.title);
+      this.history.pop();
+    }
     await this.storageService.saveData('searches', this.history);
   }
 
