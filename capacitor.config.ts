255cb073666d97c8862153127821129c02cb1848
db-99 <baj3rdav@gmail.com>
2023-01-25 15:43:23 +0100

    changed appname, ran ionic capacitor sync android --prod command

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'omdb',
+  appName: 'OMDb Search',
   webDir: 'www',
   bundledWebRuntime: false
 };
